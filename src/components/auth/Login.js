e76694550e850a931569a76c2932a40b1c7726ad
JenabB <birulangit1996@gmail.com>
2021-05-10 12:15:33 -0400

    change image to webp

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,89 +1,93 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { loginUser, useAuthState, useAuthDispatch } from '../../hook';
+import { loginUser, useAuthState, useAuthDispatch } from "../../hook";
 
 import Swal from "sweetalert2";
 
 const Login = (props) => {
-    const [data, setData] = useState({
-        email: "",
-        password: "",
-    });
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+  });
 
-    const { email, password } = data;
+  const { email, password } = data;
 
-    const dispatch = useAuthDispatch();
-    const { loading, errorMessage } = useAuthState();
+  const dispatch = useAuthDispatch();
+  const { loading, errorMessage } = useAuthState();
 
-    const handleChange = (e) => {
-        setData({ ...data, [e.target.name]: e.target.value });
-    };
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            await loginUser(dispatch, { email, password });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await loginUser(dispatch, { email, password });
 
-            Swal.fire({
-                icon: 'success',
-                text: "login success",
-                confirmButtonText: "ok"
-            })
+      Swal.fire({
+        icon: "success",
+        text: "login success",
+        confirmButtonText: "ok",
+      });
 
-            props.history.push("/user");
-            window.location.reload();
+      props.history.push("/user");
+      window.location.reload();
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        text: error.response.data.message,
+        confirmButtonText: "ok",
+      });
+    }
+  };
 
-        } catch (error) {
-            Swal.fire({
-                icon: 'error',
-                text: error.response.data.message,
-                confirmButtonText: "ok"
-            })
-        }
-    };
+  return (
+    <div className="mt-10 w-2/3 mx-auto">
+      <h4 className="text-muted text-center mb-2">Login</h4>
+      <div className="py-2 px-1">
+        <form className="text-center">
+          <div className="mt-4">
+            <h1>Email</h1>
+            <input
+              className="p-2 bg-green-400"
+              type="email"
+              name="email"
+              value={email}
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-    return (
-        <div className="mt-10 w-2/3 mx-auto">
-            <h4 className="text-muted text-center mb-2">Login</h4>
-            <div className="py-2 px-1 shadow">
-                <form className="text-center">
-                    <div className="mt-4">
-                        <h1>Email</h1>
-                        <input
-                            className="p-2 bg-green-400"
-                            type="email"
-                            name="email"
-                            value={email}
-                            onChange={handleChange}
-                            required
-                        />
-                    </div>
+          <div className="mt-4">
+            <h1>Password</h1>
+            <input
+              className="p-2 bg-green-400"
+              type="password"
+              name="password"
+              value={password}
+              onChange={handleChange}
+              required
+            />
+          </div>
 
-                    <div className="mt-4">
-                        <h1>Password</h1>
-                        <input
-                            className="p-2 bg-green-400"
-                            type="password"
-                            name="password"
-                            value={password}
-                            onChange={handleChange}
-                            required
-                        /></div>
-
-
-                    <div className="my-5">
-                        <button className="text-white bg-green-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 px-3 py-1 rounded-lg" onClick={handleSubmit}>
-                            Login
+          <div className="my-5">
+            <button
+              className="text-white bg-green-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 px-3 py-1 rounded-lg"
+              onClick={handleSubmit}
+            >
+              Login
             </button>
-                    </div>
-                    <p className="mt-3 text-center">
-                        Dont have an account?
-                            <button className="font-bold text-green-600"><Link to="/user/register">Register</Link></button>
-                    </p>
-                </form>
-            </div>
-        </div>
-    );
+          </div>
+          <p className="mt-3 text-center">
+            Dont have an account?
+            <button className="font-bold text-green-600">
+              <Link to="/user/register">Register</Link>
+            </button>
+          </p>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -4,124 +4,129 @@ import axios from "axios";
 
 import Swal from "sweetalert2";
 
-
 const Register = (props) => {
-    const [data, setData] = useState({
-        email: "",
-        password: "",
-        fullName: "",
-        contactNumber: "",
-    });
+  const [data, setData] = useState({
+    email: "",
+    password: "",
+    fullName: "",
+    contactNumber: "",
+  });
 
-    const { email, password, fullName, contactNumber } = data;
+  const { email, password, fullName, contactNumber } = data;
 
-    console.log(data);
-    const handleChange = (e) => {
-        setData({ ...data, [e.target.name]: e.target.value });
-    };
+  console.log(data);
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            setData({ ...data, error: null });
-            const res = await axios.post(
-                "https://warm-earth-68639.herokuapp.com/v1/user/register/owner",
-                {
-                    email: email,
-                    password: password,
-                    fullName: fullName,
-                    contactNumber: contactNumber,
-                },
-                {
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                }
-            );
-
-            Swal.fire({
-                icon: 'success',
-                text: res.data.message,
-                confirmButtonText: "ok"
-            })
-            props.history.push("/user/login");
-
-        } catch (error) {
-            Swal.fire({
-                icon: 'error',
-                text: error.response.data.message,
-                confirmButtonText: "ok"
-            })
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setData({ ...data, error: null });
+      const res = await axios.post(
+        "https://warm-earth-68639.herokuapp.com/v1/user/register/owner",
+        {
+          email: email,
+          password: password,
+          fullName: fullName,
+          contactNumber: contactNumber,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-    };
+      );
 
-    return (
-        <div>
-            <div className="mt-10 w-2/3 mx-auto">
-                <h4 className="mt-10 text-muted text-center mb-2">Create an account</h4>
+      Swal.fire({
+        icon: "success",
+        text: res.data.message,
+        confirmButtonText: "ok",
+      });
+      props.history.push("/user/login");
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        text: error.response.data.message,
+        confirmButtonText: "ok",
+      });
+    }
+  };
 
-                <div className="card py-2 px-1 shadow">
-                    <form className="text-center">
-                        <div className="mt-4">
-                            <h1>Email</h1>
-                            <input
-                                className="p-2"
-                                type="email"
-                                name="email"
-                                value={email}
-                                onChange={handleChange}
-                                required
-                            />
-                        </div>
-                        <div className="mt-4">
-                            <h1>Password</h1>
-                            <input
-                                className="p-2"
-                                type="password"
-                                name="password"
-                                value={password}
-                                onChange={handleChange}
-                                required
-                            />
-                        </div>
+  return (
+    <div>
+      <div className="mt-10 w-2/3 mx-auto">
+        <h4 className="mt-10 text-muted text-center mb-2">Create an account</h4>
 
-                        <div className="mt-4">
-                            <h1>Name</h1>
-                            <input
-                                className="bg-green-200 p-1"
-                                type="name"
-                                name="fullName"
-                                value={fullName}
-                                onChange={handleChange}
-
-                                required />
-                        </div>
-
-                        <div className="mt-4">
-                            <h1>contactNumber</h1>
-                            <input
-                                className="bg-green-200 p-1"
-                                type="name"
-                                name="contactNumber"
-                                value={contactNumber}
-                                onChange={handleChange}
-                                required
-                            />
-                        </div>
-
-                        <div className="text-center my-5">
-                            <button className="text-white bg-green-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 px-3 py-1 rounded-lg" onClick={handleSubmit}>
-                                Register
-            </button>
-                        </div>
-                        <p className="mt-3 text-center">
-                            Already a user? <button> <Link to="/user/login">Login</Link></button>
-                        </p>
-                    </form>
-                </div>
+        <div className="card py-2 px-1">
+          <form className="text-center">
+            <div className="mt-4">
+              <h1>Email</h1>
+              <input
+                className="p-2"
+                type="email"
+                name="email"
+                value={email}
+                onChange={handleChange}
+                required
+              />
             </div>
-        </div>
-    )
-}
+            <div className="mt-4">
+              <h1>Password</h1>
+              <input
+                className="p-2"
+                type="password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+                required
+              />
+            </div>
 
-export default Register
+            <div className="mt-4">
+              <h1>Name</h1>
+              <input
+                className="bg-green-200 p-1"
+                type="name"
+                name="fullName"
+                value={fullName}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <div className="mt-4">
+              <h1>contactNumber</h1>
+              <input
+                className="bg-green-200 p-1"
+                type="name"
+                name="contactNumber"
+                value={contactNumber}
+                onChange={handleChange}
+                required
+              />
+            </div>
+
+            <div className="text-center my-5">
+              <button
+                className="text-white bg-green-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 px-3 py-1 rounded-lg"
+                onClick={handleSubmit}
+              >
+                Register
+              </button>
+            </div>
+            <p className="mt-3 text-center">
+              Already a user?{" "}
+              <button>
+                {" "}
+                <Link to="/user/login">Login</Link>
+              </button>
+            </p>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
