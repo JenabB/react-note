5916e551321be298d43cfc6616da281a51f9d976
JenabB <birulangit1996@gmail.com>
2021-05-11 11:21:06 +0700

    fix token

--- src/components/shop/OwnerShopList.js
+++ src/components/shop/OwnerShopList.js
@@ -1,42 +1,46 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+
+import { useAuthState } from "../../hook";
 
 const OwnerShopList = () => {
-    const [shopList, setShopList] = useState([])
+  const [shopList, setShopList] = useState([]);
 
-    useEffect(() => {
+  const user = useAuthState();
 
-        axios.get(`https://warm-earth-68639.herokuapp.com/v1/shop`,
-            {
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${localStorage.getItem("token")}`
-                },
-            }).then(result => {
-                console.log(result)
-                setShopList(result.data.data)
-            })
-    })
+  useEffect(() => {
+    axios
+      .get(`https://warm-earth-68639.herokuapp.com/v1/shop`, {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${user.token}`,
+        },
+      })
+      .then((result) => {
+        setShopList(result.data.data);
+      });
+  });
 
-    console.log(shopList)
-    return (
-        <div className="lg:w-2/3 mx-auto sm:w-full">
-            {shopList.length > 0 ? (
-                <div>
-                    {shopList.map((shop, index) => (
-                        <Link to={`user/shop/detail/${shop.shopId}`}>
-                            <div key={index} className="shadow-lg m-3 p-3">
-                                <h1 className="font-bold">{shop.shopName}</h1>
-                                <h2>{shop.addressDetail}</h2>
-                                <h3>{shop.contactNumber}</h3>
-                            </div>
-                        </Link>
-                    ))}
-                </div>
-            ) : (<h1 className="text-center py-12">No Shop Yet</h1>)}
+  return (
+    <div className="lg:w-2/3 mx-auto sm:w-full">
+      {shopList.length > 0 ? (
+        <div>
+          {shopList.map((shop, index) => (
+            <Link to={`user/shop/detail/${shop.shopId}`}>
+              <div key={index} className="shadow-lg m-3 p-3">
+                <h1 className="font-bold">{shop.shopName}</h1>
+                <h2>{shop.addressDetail}</h2>
+                <h3>{shop.contactNumber}</h3>
+              </div>
+            </Link>
+          ))}
         </div>
-    )
-}
+      ) : (
+        <h1 className="text-center py-12">No Shop Yet</h1>
+      )}
+    </div>
+  );
+};
 
-export default OwnerShopList
+export default OwnerShopList;
