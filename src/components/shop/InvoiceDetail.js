21e8aa05352ea75fb811d8af0a995957697c4bde
JenabB <birulangit1996@gmail.com>
2021-05-14 18:17:40 +0700

    s

--- src/components/shop/InvoiceDetail.js
+++ src/components/shop/InvoiceDetail.js
@@ -1,10 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import axios from "axios";
-import { useAuthState } from "../../hook";
 
 const InvoiceDetail = () => {
-  const [invoiceDetail, setinvoiceDetail] = useState([]);
-
   useEffect(() => {
     axios
       .get(`https://warm-earth-68639.herokuapp.com/v1/shop/42/invoice/3`, {
