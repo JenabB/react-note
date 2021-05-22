1c01639abd14cf07bc7b74cd4022ab1fd66fb52d
JenabB <birulangit1996@gmail.com>
2021-05-22 18:50:59 +0700

    kagak pake modal tong

--- src/components/auth/ChangePassword.js
+++ src/components/auth/ChangePassword.js
@@ -4,10 +4,9 @@ import { logout, useAuthDispatch, useAuthState } from "../../hook";
 import Swal from "sweetalert2";
 import NavWithBack from "../NavWithBack";
 import axios from "axios";
-// import Modal from "react-modal";
 import { useHistory } from "react-router-dom";
 
-const ChangePassword = (props) => {
+const ChangePassword = () => {
   const [data, setData] = useState({
     password: "",
     newPassword: "",
@@ -16,27 +15,14 @@ const ChangePassword = (props) => {
   const [loading, setLoading] = useState("change");
   const { password, newPassword } = data;
   console.log("data", data);
-  // const [isOpen, setIsOpen] = useState(false);
 
   const dispatch = useAuthDispatch();
   const user = useAuthState();
 
   let history = useHistory();
-
-  // function goBack() {
-  //   history.goBack(-3);
-  // }
   const handleChange = (e) => {
     setData({ ...data, [e.target.name]: e.target.value });
   };
-
-  // const openModal = () => {
-  //   setIsOpen(true);
-  // };
-
-  // const closeModal = () => {
-  //   setIsOpen(false);
-  // };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -59,12 +45,6 @@ const ChangePassword = (props) => {
           });
         });
 
-      // if (res) {
-
-      //   props.history.push("/user");
-      // } else {
-
-      // }
       setLoading("change");
     } catch (error) {
       console.log(error.response);
@@ -124,32 +104,13 @@ const ChangePassword = (props) => {
             />
           </div>
 
-          <input type="submit" value={loading} />
-          {/* <div className="my-5">
-            <button
-              onClick={openModal}
+          <div className="my-5">
+            <input
+              type="submit"
               className="text-white bg-blue-700 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 px-3 py-1 rounded-lg"
-            >
-              Change
-            </button>
-          </div> */}
-
-          {/* <Modal isOpen={isOpen} onRequestClose={closeModal}>
-            <div className="py-20">
-              <h1 className="text-center text-2xl py-8">Change Password</h1>
-              <div className="flex justify-center">
-                <button className="text-white bg-blue-700 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 px-3 py-1 rounded-lg">
-                  {loading}
-                </button>
-                <button
-                  className="shadow-lg bg-gray-200 px-4 py-2 rounded"
-                  onClick={closeModal}
-                >
-                  No
-                </button>
-              </div>
-            </div>
-          </Modal> */}
+              value={loading}
+            />
+          </div>
         </form>
       </div>
     </div>
